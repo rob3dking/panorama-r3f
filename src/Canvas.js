aa578c7e446b4fc3113ccf35f992935b32c03d64
rob3dking <robertw98218@gmail.com>
2024-02-29 16:54:49 -0500

    - implement the panorama room with works for removing artifacts in pole.

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,22 +1,25 @@
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber'
-import { Environment, OrbitControls } from '@react-three/drei'
+import { Environment, OrbitControls, useTexture } from '@react-three/drei'
 
 export const App = ({ position = [0, 1, 2.5], fov = 25 }) => (
   <Canvas shadows camera={{ position, fov }} gl={{ preserveDrawingBuffer: true }} eventSource={document.getElementById('root')} eventPrefix="client">
     <ambientLight intensity={0.5} />
     <Environment preset='city' />
-    <Model />
-    <OrbitControls />
+    <PanoRoom />
+    <OrbitControls enableZoom={false} enablePan={false} enableDamping dampingFactor={0.2}/>
   </Canvas>
 )
 
-const Model = () => {
+const PanoRoom = () => {
+  const texture = useTexture('./image.jpg');
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  texture.flipY = -1;
   return (
-    <mesh
-      geometry={new THREE.BoxGeometry(1, 1, 1)}
-      material={new THREE.MeshStandardMaterial({ color: 'red' })}
-    />
+    <mesh>
+      <sphereGeometry args={[800, 60, 40]} />
+      <meshBasicMaterial envMap={texture} side={THREE.BackSide} />
+    </mesh>
   )
 }
 
