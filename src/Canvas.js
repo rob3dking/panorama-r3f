5ec4b5b27af85e687d6cde091c705f085321dc9a
rob3dking <robertw98218@gmail.com>
2024-02-29 17:28:26 -0500

    - basic pano room implemented without pole artifacts

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,9 +1,10 @@
 import * as THREE from 'three';
-import { Canvas } from '@react-three/fiber'
-import { Environment, OrbitControls, useTexture } from '@react-three/drei'
+import { Canvas, useLoader } from '@react-three/fiber'
+import { Environment, OrbitControls } from '@react-three/drei'
 
-export const App = ({ position = [0, 1, 2.5], fov = 25 }) => (
-  <Canvas shadows camera={{ position, fov }} gl={{ preserveDrawingBuffer: true }} eventSource={document.getElementById('root')} eventPrefix="client">
+export const App = () => (
+  
+  <Canvas frameloop="demand" shadows eventSource={document.getElementById('root')} eventPrefix="client">
     <ambientLight intensity={0.5} />
     <Environment preset='city' />
     <PanoRoom />
@@ -12,15 +13,13 @@ export const App = ({ position = [0, 1, 2.5], fov = 25 }) => (
 )
 
 const PanoRoom = () => {
-  const texture = useTexture('./image.jpg');
-  texture.mapping = THREE.EquirectangularReflectionMapping;
+  const texture = useLoader( THREE.TextureLoader, './image.jpg');
   texture.flipY = -1;
+  texture.mapping = THREE.EquirectangularReflectionMapping;
   return (
     <mesh>
-      <sphereGeometry args={[800, 60, 40]} />
+      <sphereGeometry args={[500, 60, 40]} />
       <meshBasicMaterial envMap={texture} side={THREE.BackSide} />
     </mesh>
   )
 }
-
-
